ffb9824dd0fc616ee044fc3d4c41ddada1c1ee54
Jonathan Varela <jonvarela4@gmail.com>
2024-02-20 22:13:03 -0500

    first commit

---
+++ api/posts.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const posts = await prisma.post.findMany();
+    res.json(posts);
+  } else if (req.method === 'POST') {
+    const { title, content } = req.body;
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    res.json(post);
+  }
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>My Blog Home page</h1>
+    </main>
+  );
+}
